fa7172e01bca4bdaa135cacff0ebd68d2c8784e1
Yesha Mavani <yesha.mavani@sourcefuse.com>
2024-10-21 17:29:37 +0530

    fix(my-facade-facade): update the tenant mgmt service tar
    GH-00

--- facades/tenant-mgmt-facade/src/services/tenant-helper.service.ts
+++ facades/tenant-mgmt-facade/src/services/tenant-helper.service.ts
@@ -157,22 +157,25 @@ export class TenantHelperService {
       `Bearer ${token.replace(/^Bearer\s+/i, '')}`,
       new TenantOnboardDTO(dto),
     );
+    /**for market place we assume IdP will always be cognito
+     * if we call the tenant config creation api make sure to add its permission
+     * in the token
+     */
+    // const config = new TenantMgmtConfig({
+    //   configKey: 'auth0',
+    //   configValue: {
+    //     password: 'test123@123',
+    //     connection: 'Username-Password-Authentication',
+    //     display_name: 'corporatidonw',
+    //     verify_email: true,
+    //     page_background: '#000000',
+    //     primary_color: '#0059d6',
+    //   },
+    //   tenantId: tenant.id,
+    // });
 
-    const config = new TenantMgmtConfig({
-      configKey: 'auth0',
-      configValue: {
-        password: 'test123@123',
-        connection: 'Username-Password-Authentication',
-        display_name: 'corporatidonw',
-        verify_email: true,
-        page_background: '#000000',
-        primary_color: '#0059d6',
-      },
-      tenantId: tenant.id,
-    });
-
-    await this.tenantMgmtProxyService.createTenantConfig(token, config);
-    console.log('step 2');
+    // await this.tenantMgmtProxyService.createTenantConfig(token, config);
+    // console.log('step 2');
 
     const customer: CustomerDtoType = {
       firstName: tenant.contacts[0].firstName,
