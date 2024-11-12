dacf271a12ff9f806374cc1ef174172328c39112
Yesha Mavani <yesha.mavani@sourcefuse.com>
2024-11-12 16:13:20 +0530

    chore(my-facade-facade): rebase
    GH-33
    
    GH-33

---
+++ services/user-tenant-service/src/lambda.ts
@@ -0,0 +1,36 @@
+import {AnyObject} from '@loopback/repository';
+const serverlessExpress = require('@vendia/serverless-express');
+
+export * from './application';
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let serverlessApp: (arg0: AnyObject, arg1: unknown) => any; // NOSONAR
+
+export const PORT = 3000;
+
+export async function setup(event: AnyObject, context: unknown) {
+  const {UserTenantService} = require('./application');
+  const config = {
+    rest: {
+      port: +(process.env.PORT ?? PORT),
+      host: process.env.HOST,
+      gracePeriodForClose: 5000, // 5 seconds
+      openApiSpec: {
+        // useful when used with OpenAPI-to-GraphQL to locate your application
+        setServersFromRequest: true,
+      },
+    },
+  };
+  const app = new UserTenantService(config);
+  await app.boot();
+  const requestHandler = app.restServer.requestHandler;
+  serverlessApp = serverlessExpress({app: requestHandler});
+  return serverlessApp(event, context);
+}
+
+export const handler = async (event: AnyObject, context: unknown) => {
+  if (serverlessApp) {
+    return serverlessApp(event, context);
+  }
+
+  return setup(event, context);
+};
